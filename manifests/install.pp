2f87e9b0aca8401ceea9d82b9fa91285d77cb590
Benjamin Grewell <benjamin.grewell@intel.com>
2023-03-09 10:28:19 -0800

    add netcheck to cluster_tools

--- manifests/install.pp
+++ manifests/install.pp
@@ -6,8 +6,14 @@
 #   include cluster_tools::install
 class cluster_tools::install {
   # Setup the .profile skeleton file
+  $netcheck_desired_state = $::cluster_tools::netcheck_ensure ? {
+    'absent'      => 'absent',
+    'removed'     => 'absent',
+    'uninstalled' => 'absent',
+    default       => 'file',
+  }
   file { '/usr/local/bin/netcheck':
-    ensure => file,
+    ensure => $netcheck_desired_state,
     owner  => 'root',
     group  => 'root',
     mode   => '0755',
