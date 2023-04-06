95e1ceb951fc321ce31b76f2ff053b4116866ba2
Benjamin Grewell <benjamin.grewell@intel.com>
2023-04-06 11:50:34 -0700

    modify config logic

--- manifests/config.pp
+++ manifests/config.pp
@@ -20,13 +20,20 @@ class cluster_tools::config {
     }
 
     $certname = $facts['networking']['fqdn']
-    if $group_identifier != "unknown" {
-      # Override to all users on management hosts
-      $identifier_value = $facts['mgmt_host'] ? {
-        false   => $group_identifier,
-        default => $facts['subproject'],
+    if $::cluster_tools::pp_project != "unknown" {
+      # Check if this is a subproject host managed by the shared puppet
+      $subproject = $facts['subproject']
+
+      $project_id = $subproject ? {
+        'shared' => 'shared',
+        'niwc'   => '064',
+        'lenovo' => '065',
+        'wsr'    => '066',
+        'ido'    => '067',
+        default  => 'unknown',
       }
-      notice(sprintf('>> %-40s: SUBPROJECT | identifier_value = %-20s', $certname, $identifier_value))
+
+      notice(sprintf('>> %-40s: SUBPROJECT DEBUG | subproject = %-20s | project_id = %-10s', $certname, $subproject, $project_id))
 
       file { '/etc/security/access.conf':
         ensure  => file,
@@ -34,7 +41,7 @@ class cluster_tools::config {
         group   => 'root',
         mode    => '0644',
         content => epp('cluster_tools/access/access.epp', {
-          'group_identifier' => $identifier_value,
+          'group_identifier' => $project_id,
         }),
       }
 
@@ -46,7 +53,7 @@ class cluster_tools::config {
         replace => false,
       }
     } else {
-      warning("${::hostname}: Unknown project_id value = '${facts['project_id']}'")
+      warning("${::hostname}: Unknown project_id value = '${::cluster_tools::pp_project}'")
     }
 
   } else {
