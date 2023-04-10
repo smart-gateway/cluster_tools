944ef8a993ed1b11dace052b6ad72e8ed5d3a891
Benjamin Grewell <benjamin.grewell@intel.com>
2023-04-10 10:47:27 -0700

    modify to 'detect' p10k.zsh

--- manifests/ohmyzsh/cluster_banner.pp
+++ manifests/ohmyzsh/cluster_banner.pp
@@ -10,21 +10,25 @@ define cluster_tools::ohmyzsh::cluster_banner(
   String  $user_name,
 ) {
 
-  file_line { "ensure cluster_banner function exists for ${user_name}":
+  exec { "dummy command to ensure ~/.p10k.zsh exists for ${user_name}":
+    command => "true",
+    path => "/usr/bin:/usr/sbin:/bin",
+    onlyif => "test -f /home/${user_name}/.p10k.zsh",
+  }
+
+  -> file_line { "ensure cluster_banner function exists for ${user_name}":
     ensure => present,
-    path   => "/home/$user_name/.p10k.zsh",
+    path   => "/home/${user_name}/.p10k.zsh",
     match  => '^  function prompt_cluster_banner',
     line   => "  function prompt_cluster_banner() { p10k segment -f 32 -i '⧉' -t '${project_name}-${cluster_name}' }",
     after  => "# typeset -g POWERLEVEL9K_TIME_PREFIX='%fat '",
   }
 
-  file_line { "ensure cluster_banner function enabled for ${user_name}":
+  -> file_line { "ensure cluster_banner function enabled for ${user_name}":
     ensure => present,
-    path   => "/home/$user_name/.p10k.zsh",
+    path   => "/home/${user_name}/.p10k.zsh",
     match  => "    cluster_banner",
     line   => "    cluster_banner",
     after  => "^    # example user-defined segment",
   }
-
-
 }
